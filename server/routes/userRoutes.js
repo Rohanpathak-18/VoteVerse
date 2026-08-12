8929ccf48bc204772c0441569bd74c6fda15f761
rohan pathak <rohanpathak0171@gmail.com>
2026-08-13 01:47:20 +0530

    Fix model import casing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user");
 const Candidate = require("../models/candidate");
+const User = require("../models/user");
 
 const {
   jwtAuthMiddleware,
