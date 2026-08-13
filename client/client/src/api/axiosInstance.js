4ac56ca60f9df12e0656c021824c816bb42dd0cf
rohan pathak <rohanpathak0171@gmail.com>
2026-08-13 16:04:41 +0530

    Fix production API base URL

--- client/client/src/api/axiosInstance.js
+++ client/client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://voteverse-lft7.onrender.com",
+  baseURL: "https://voteverse-lft7.onrender.com/api",
   withCredentials: true,
 });
 
